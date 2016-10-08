b6ba7f04ec1614838c5a48386ae3f687ddd2452d
Redim <1153710218@stu.hit.edu.cn>
2016-10-08 21:04:44 +0800

    2016-10-8

---
+++ Task3/15/R_costFunction.m
@@ -0,0 +1,6 @@
+function [J, grad] = costFunction(theta, X, Y)
+    [m, n] = size(X);
+    h = ones(m,1)./(1+exp(-X*theta));
+    J = (-sum(Y.*log(h))+sum((Y-1).*log(1-h)))/m;
+    grad = X'*(h-Y)/m;
+end
